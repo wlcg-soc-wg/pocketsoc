9d6c0669b4686aa5617c9a563fee4424f145a4fa
David Crooks <dcrooks@stfc-staff-0553.rl.ac.uk>
2018-07-23 16:23:36 +0100

    Normalising local data storage

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ docker network create --subnet 172.18.0.0/16 internal
 docker network create --subnet 172.19.0.0/16 external
 docker network create --subnet 172.20.0.0/16 mirror
 
+mkdir -p ./misp-data/{misp,mysql}
+
 docker-compose build
 docker-compose up -d

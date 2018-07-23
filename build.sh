901036a772317645f495efb286e445058347664e
David Crooks <dcrooks@stfc-staff-0553.rl.ac.uk>
2018-07-23 15:22:14 +0100

    Handy build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+docker network create --subnet 172.18.0.0/16 internal
+docker network create --subnet 172.19.0.0/16 external
+docker network create --subnet 172.20.0.0/16 mirror
+
+docker-compose build
+docker-compose up -d

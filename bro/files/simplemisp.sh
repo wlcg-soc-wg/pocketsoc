918c118e9a50569cefc1159b52de14892eac9518
David Crooks <david.crooks@glasgow.ac.uk>
2018-07-30 12:50:03 +0100

    Simplify simplemisp.sh for easier auth key replacement

--- bro/files/simplemisp.sh
+++ bro/files/simplemisp.sh
@@ -6,6 +6,6 @@ FEEDFILE=$FEEDDIR"/testdata.txt"
 mkdir -p $FEEDDIR
 
 touch $FEEDFILE
-AUTH_KEY="Authorization: mpl2OY4GQ9YMIv4pb2jtLvlKX6HblKwl5m6AYkBM"
+AUTH_KEY="Authorization: XXXXXXXX"
 
 curl -s --header "$AUTH_KEY" http://172.20.0.20/attributes/bro/download/ > $FEEDFILE

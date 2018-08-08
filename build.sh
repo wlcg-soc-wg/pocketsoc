4a00a7d625abcd2e635759b23e427eee705e5362
David Crooks <dcrooks@Davids-MacBook-Pro.local>
2018-08-08 14:20:27 +0100

    Decoupling with configure.sh

--- build.sh
+++ build.sh
@@ -10,11 +10,6 @@ docker network create --subnet 172.18.0.0/16 internal
 docker network create --subnet 172.19.0.0/16 external
 docker network create --subnet 172.20.0.0/16 mirror
 
-# Clean MISP data
-
-rm -rf ./volumes/{misp-web,misp-db}
-mkdir -p ./volumes/{misp-web,misp-db}
-
 # Configure logstash pipeline(s) and MISP
 
 ./configure.sh

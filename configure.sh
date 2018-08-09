150d0416c0d46d6678a37cecc96022e2638d3737
David Crooks <dcrooks@Davids-MacBook-Pro.local>
2018-08-09 10:43:19 +0100

    Structure

--- configure.sh
+++ configure.sh
@@ -13,16 +13,10 @@ sed -i.bak "/${TOFIND}/r ${CONFIGFILE}"  $FILETOMOD
 
 # elastic
 
-mkdir -p ./{data,volumes}/{elasticsearch,logstash,kibana}/config/
-mkdir -p ./volumes/logstash/pipeline/
+mkdir -p ./volumes/{elasticsearch,logstash,kibana}/config/
 
 # Refresh core elastic config from submodule
 
-cp ./docker-elk/elasticsearch/config/elasticsearch.yml ./data/elasticsearch/config/
-cp ./docker-elk/logstash/config/logstash.yml ./data/logstash/config/
-cp ./docker-elk/kibana/config/kibana.yml ./data/kibana/config/
-
-cp ./data/elasticsearch/config/elasticsearch.yml ./volumes/elasticsearch/config/elasticsearch.yml
-cp ./data/logstash/config/logstash.yml ./volumes/logstash/config/logstash.yml
-cp ./data/logstash/pipeline/logstash.conf ./volumes/logstash/pipeline/logstash.conf
-cp ./data/kibana/config/kibana.yml ./volumes/kibana/config/kibana.yml
+cp ./docker-elk/elasticsearch/config/elasticsearch.yml ./volumes/elasticsearch/config/
+cp ./docker-elk/logstash/config/logstash.yml ./volumes/logstash/config/
+cp ./docker-elk/kibana/config/kibana.yml ./volumes/kibana/config/

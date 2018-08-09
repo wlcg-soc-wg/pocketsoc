24eda10d3f664927ed1d623ba6d28cde8bbd955f
David Crooks <dcrooks@Davids-MacBook-Pro.local>
2018-08-09 11:17:28 +0100

    Moving volume directory for submodules to external directory

--- configure.sh
+++ configure.sh
@@ -5,19 +5,19 @@
 ## Patch misp-web Dockerfile to personalise MISP config
 
 TOFIND="RUN sudo -u www-data cp -a \/var\/www\/MISP\/app\/Config\/config.default.php \/var\/www\/MISP\/app\/Config\/config.php"
-FILETOMOD="misp-docker/misp-web/Dockerfile"
-CONFIGFILE="./data/misp-web/misp-configure.txt"
+FILETOMOD="external/misp-docker/misp-web/Dockerfile"
+CONFIGFILE="./components/misp-web/misp-configure.txt"
 
 sed -i.bak "/${TOFIND}/r ${CONFIGFILE}"  $FILETOMOD
 
 ## Make sure volume directories are in place
 
-mkdir -p ./volumes/{misp-web,misp-db}
+mkdir -p ./external/volumes/{misp-web,misp-db}
  
 # Refresh core elastic config from submodule
 
-mkdir -p ./volumes/{elasticsearch,logstash,kibana}/config/
+mkdir -p ./external/volumes/{elasticsearch,logstash,kibana}/config/
 
-cp ./docker-elk/elasticsearch/config/elasticsearch.yml ./volumes/elasticsearch/config/
-cp ./docker-elk/logstash/config/logstash.yml ./volumes/logstash/config/
-cp ./docker-elk/kibana/config/kibana.yml ./volumes/kibana/config/
+cp ./external/docker-elk/elasticsearch/config/elasticsearch.yml ./external/volumes/elasticsearch/config/
+cp ./external/docker-elk/logstash/config/logstash.yml ./external/volumes/logstash/config/
+cp ./external/docker-elk/kibana/config/kibana.yml ./external/volumes/kibana/config/

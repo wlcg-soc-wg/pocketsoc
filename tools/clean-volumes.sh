58243b7ad52dc6a04f58a92644466d66f2b2c3c9
David Crooks <dcrooks@stfc-staff-0473.rl.ac.uk>
2018-08-14 10:05:08 +0100

    consolidate to one clean command

--- tools/clean-volumes.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Clean MISP data
-
-rm -rf ./volumes/{misp-web,misp-db}
-
-# Clean ELK data
-
-rm -rf ./volumes/{elasticsearch,logstash,kibana}
--- tools/clean.sh
+++ tools/clean.sh
@@ -1,9 +1,22 @@
 #! /bin/bash
 
+echo "Bringing down docker containers..."
 docker-compose down
 
+echo "Removing docker networks..."
 docker network rm internal
 docker network rm external
 docker network rm mirror
 
+echo "Cleaning rules..."
 git checkout components/elastalert/config/rules/new_term.yaml
+
+# Clean MISP data
+
+echo "Cleaning MISP data to default..."
+rm -rf ./volumes/{misp-web,misp-db}
+
+# Clean ELK data
+
+echo "Cleaning elastic data to default..."
+rm -rf ./volumes/{elasticsearch,logstash,kibana}

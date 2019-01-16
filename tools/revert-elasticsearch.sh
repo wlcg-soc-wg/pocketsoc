f055dfba75bf63ad8ba3fdbb530e8dd33d57aa73
David Crooks <david.crooks@stfc.ac.uk>
2019-01-16 11:55:26 +0000

    Generalised and error checking

--- tools/revert-elasticsearch.sh
+++ tools/revert-elasticsearch.sh
@@ -1,5 +1,23 @@
 #! /bin/bash
 
-docker exec elasticsearch curl -s -X DELETE "localhost:9200/logstash*"
-docker exec elasticsearch curl -s -X DELETE "localhost:9200/elastalert_status*"
-docker exec elastalert elastalert-create-index --config /config/config.yaml
+elastic_state=`docker inspect -f {{.State.Running}} elasticsearch`
+
+while [[ $elastic_state == "false" ]]; do
+    echo "Waiting for elasticsearch"
+    sleep 10
+    elastic_state=`docker inspect -f {{.State.Running}} elasticsearch`
+done
+
+echo "Elasticsearch up"
+
+indices=`docker exec elasticsearch curl -s -X GET "localhost:9200/_cat/indices?v" | grep -v health | awk '{print $3}' | cut -d- -f1`
+
+for index in $indices; do
+    echo $index
+    docker exec elasticsearch curl -s -X DELETE "localhost:9200/$index*"
+    echo
+done
+
+remaining=`docker exec elasticsearch curl -s -X GET "localhost:9200/_cat/indices?v" | grep -v health | awk '{print $3}' | wc -l`
+
+echo "Indices remaining = "$remaining

70bfcf6121d69487ac1cb11c6040744aeccad67b
David Crooks <david.crooks@stfc.ac.uk>
2019-01-16 11:55:04 +0000

    Added pocketsoc prefix

--- tools/purge.sh
+++ tools/purge.sh
@@ -2,6 +2,6 @@
 
 docker-compose down
 
-docker image ls | egrep "misp|demo" | awk '{print $3}' | xargs -n1 -t -I {} docker image rm -f {}
+docker image ls | egrep "misp|demo|pocketsoc" | awk '{print $3}' | xargs -n1 -t -I {} docker image rm -f {}
 
 tools/clean-volumes.sh

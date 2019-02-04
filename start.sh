03b77a7ccc7d549c2efa33ef2231796905732d91
David Crooks <david.crooks@stfc.ac.uk>
2019-02-04 11:41:29 +0000

    Full path for docker-compose

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+scale=$1
+
 logfile="log/pocketsoc_run.log"
 
 ./configure.sh
 
 echo "Starting up, see $logfile"
-docker-compose up > $logfile 2>&1 &
+/usr/local/bin/docker-compose up --scale apache=$scale > $logfile 2>&1 &

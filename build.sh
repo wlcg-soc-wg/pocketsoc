316b2d66f5b36f82162c002c4e0949d77d1e2093
David Crooks <david.crooks@stfc.ac.uk>
2019-02-14 17:32:55 +0000

    Switching to SOCDIR

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 
 scale=$1
 
-logfile="log/build.log"
+logfile=$SOCDIR"/log/build.log"
 
-./configure.sh
+$SOCDIR/configure.sh
 
 echo "Starting up, see $logfile"
+
+cd $SOCDIR
+
 /usr/local/bin/docker-compose build  2>&1 | tee $logfile

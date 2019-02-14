711c3ed4fec2d673bf4aa50c387a344ff1e1c7b8
David Crooks <david.crooks@stfc.ac.uk>
2019-02-14 14:58:30 +0000

    Add cleaning switch to remove orphans

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ logfile="log/pocketsoc_run.log"
 ./configure.sh
 
 echo "Starting up, see $logfile"
-/usr/local/bin/docker-compose up --scale apache=$scale > $logfile 2>&1 &
+/usr/local/bin/docker-compose up --remove-orphans --scale apache=$scale > $logfile 2>&1 &
